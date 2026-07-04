6b790a346136b3fa36cab1b67a4d000215c2a0af
MD SHAHFAHAD KHAN <168389196+shahfahad09@users.noreply.github.com>
2026-07-05 01:09:42 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -8,27 +8,19 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 from sklearn.pipeline import Pipeline
 
 
-# ============================
 # Paths
-# ============================
 DATA_PATH = r"D:\ML PROJECTS\CustomerChurnPrediction\Data\cleaned_customer_churn.csv"
 MODEL_DIR = r"D:\ML PROJECTS\CustomerChurnPrediction\models"
 
 os.makedirs(MODEL_DIR, exist_ok=True)
 
-
-# ============================
-# Load Data
-# ============================
 df = pd.read_csv(DATA_PATH)
 
 print("Dataset Loaded Successfully")
 print("Shape:", df.shape)
 
 
-# ============================
 # Separate Features and Target
-# ============================
 X = df.drop("Churn", axis=1)
 y = df["Churn"]
 
@@ -40,9 +32,7 @@ y = target_encoder.fit_transform(y)
 joblib.dump(target_encoder, os.path.join(MODEL_DIR, "target_encoder.pkl"))
 
 
-# ============================
-# Identify Column Types
-# ============================
+
 categorical_cols = X.select_dtypes(include=["object"]).columns.tolist()
 numerical_cols = X.select_dtypes(include=["int64", "float64"]).columns.tolist()
 
@@ -53,9 +43,7 @@ print("\nNumerical Columns:")
 print(numerical_cols)
 
 
-# ============================
-# Train-Test Split
-# ============================
+
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -68,9 +56,6 @@ print("\nTrain Shape:", X_train.shape)
 print("Test Shape:", X_test.shape)
 
 
-# ============================
-# Preprocessing Pipeline
-# ============================
 categorical_pipeline = Pipeline(
     steps=[
         ("onehot", OneHotEncoder(handle_unknown="ignore"))
@@ -91,9 +76,8 @@ preprocessor = ColumnTransformer(
 )
 
 
-# ============================
 # Fit and Transform Data
-# ============================
+
 X_train_processed = preprocessor.fit_transform(X_train)
 X_test_processed = preprocessor.transform(X_test)
 
@@ -102,18 +86,14 @@ print("Processed Train Shape:", X_train_processed.shape)
 print("Processed Test Shape:", X_test_processed.shape)
 
 
-# ============================
-# Save Processed Data
-# ============================
+
 joblib.dump(X_train_processed, os.path.join(MODEL_DIR, "X_train_processed.pkl"))
 joblib.dump(X_test_processed, os.path.join(MODEL_DIR, "X_test_processed.pkl"))
 joblib.dump(y_train, os.path.join(MODEL_DIR, "y_train.pkl"))
 joblib.dump(y_test, os.path.join(MODEL_DIR, "y_test.pkl"))
 
 
-# ============================
-# Save Preprocessor
-# ============================
+
 joblib.dump(preprocessor, os.path.join(MODEL_DIR, "preprocessor.pkl"))
 
 print("\nAll files saved successfully in models folder.")
